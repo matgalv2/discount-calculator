b5ad64ed0e73613f2dd693c078e5ce4e329de338
mateusz <matgalv2@gmail.com>
2024-10-18 21:15:57 +0200

    Added discount service definition

---
+++ src/main/java/io/github/g4lowy/service/DiscountCalculationService.java
@@ -0,0 +1,10 @@
+package io.github.g4lowy.service;
+
+import io.github.g4lowy.domain.Item;
+
+import java.util.List;
+import java.util.Map;
+
+public interface DiscountCalculationService {
+    Map<String, Integer> getDiscounts(List<Item> items, int discount);
+}
