19629162268c2e0bb1235404780113198849fdcf
mateusz <matgalv2@gmail.com>
2024-10-18 21:16:04 +0200

    Added discount service impl

---
+++ src/main/java/io/github/g4lowy/service/DiscountCalculationDefaultService.java
@@ -0,0 +1,31 @@
+package io.github.g4lowy.service;
+
+import io.github.g4lowy.strategies.DiscountAlgorithm;
+import io.github.g4lowy.strategies.DiscountStrategy;
+import io.github.g4lowy.domain.Item;
+import io.github.g4lowy.strategies.ProportionalRestForLastDiscount;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class DiscountCalculationDefaultService implements DiscountCalculationService {
+
+    private final Map<DiscountStrategy, DiscountAlgorithm> algorithms;
+    private static final DiscountStrategy DEFAULT_STRATEGY = DiscountStrategy.PROPORTIONAL;
+
+
+    public DiscountCalculationDefaultService() {
+        algorithms = new HashMap<>();
+        algorithms.put(DiscountStrategy.PROPORTIONAL, new ProportionalRestForLastDiscount());
+    }
+
+
+    public Map<String, Integer> getDiscounts(List<Item> items, int discount){
+        return algorithms.get(DEFAULT_STRATEGY).calculate(items, discount);
+    }
+
+    public Map<String, Integer> getDiscounts(List<Item> items, int discount, DiscountStrategy discountStrategy){
+        return algorithms.get(discountStrategy).calculate(items, discount);
+    }
+}
