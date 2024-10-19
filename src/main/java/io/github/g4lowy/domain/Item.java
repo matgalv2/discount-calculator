670e18ecf9ed89b8daa9c8abeea78d5fb56792aa
mateusz <matgalv2@gmail.com>
2024-10-19 20:28:27 +0200

    Updated Item

--- src/main/java/io/github/g4lowy/domain/Item.java
+++ src/main/java/io/github/g4lowy/domain/Item.java
@@ -1,4 +1,24 @@
 package io.github.g4lowy.domain;
 
 
-public record Item(String key, int value) {}
+import lombok.Getter;
+
+import java.util.Optional;
+
+@Getter
+public class Item {
+    private final String key;
+    private final int value;
+
+    private Item(String key, int value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public static Optional<Item> of(String key, int value) {
+        if(value > 0)
+            return Optional.of(new Item(key, value));
+        else
+            return Optional.empty();
+    }
+}
