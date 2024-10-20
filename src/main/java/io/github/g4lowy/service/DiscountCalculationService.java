174a87dabc2dc9c81b7265d0122b9d76d98369d6
mateusz <matgalv2@gmail.com>
2024-10-20 19:28:57 +0200

    Changed signature

--- src/main/java/io/github/g4lowy/service/DiscountCalculationService.java
+++ src/main/java/io/github/g4lowy/service/DiscountCalculationService.java
@@ -1,13 +1,12 @@
 package io.github.g4lowy.service;
 
-import io.github.g4lowy.domain.Discount;
-import io.github.g4lowy.domain.Item;
+import io.github.g4lowy.dto.Data;
+import io.github.g4lowy.error.DomainValidationException;
 import io.github.g4lowy.strategy.DiscountStrategy;
 
-import java.util.List;
 import java.util.Map;
 
 public interface DiscountCalculationService {
-    Map<String, Integer> getDiscounts(List<Item> items, Discount discount);
-    Map<String, Integer> getDiscounts(List<Item> items, Discount discount, DiscountStrategy discountStrategy);
+    Map<String, Integer> getDiscounts(Data data) throws DomainValidationException;
+    Map<String, Integer> getDiscounts(Data data, DiscountStrategy discountStrategy) throws DomainValidationException;
 }
