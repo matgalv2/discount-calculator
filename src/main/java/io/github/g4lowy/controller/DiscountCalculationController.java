4fcac0be3b4c8701a54f592c843df3ee2158bc69
mateusz <matgalv2@gmail.com>
2024-10-20 19:29:38 +0200

    Moved validation logic to service

--- src/main/java/io/github/g4lowy/controller/DiscountCalculationController.java
+++ src/main/java/io/github/g4lowy/controller/DiscountCalculationController.java
@@ -1,71 +1,20 @@
 package io.github.g4lowy.controller;
 
-import io.github.g4lowy.domain.Discount;
-import io.github.g4lowy.domain.DomainValidationException;
+import io.github.g4lowy.error.DomainValidationException;
 import io.github.g4lowy.dto.Data;
 import io.github.g4lowy.service.DiscountCalculationService;
 import lombok.RequiredArgsConstructor;
 
-import java.util.List;
 import java.util.Map;
-import java.util.Optional;
-
-import io.github.g4lowy.domain.Item;
 
 @RequiredArgsConstructor
 public class DiscountCalculationController {
 
     private final DiscountCalculationService discountCalculationService;
 
+    // wyniesienie walidacji
     public Map<String, Integer> calculateDiscounts(Data data) throws DomainValidationException {
-        if(data == null || data.items() == null)
-            throw new DomainValidationException("Data.items is null");
-
-        Optional<Discount> discountOps = Discount.of(data.discount());
-
-        List<Optional<Item>> itemsOps =
-                data.items()
-                        .entrySet()
-                        .stream()
-                        .map(entry -> Item.of(entry.getKey(), entry.getValue()))
-                        .toList();
-
-        if(discountOps.isEmpty() || itemsOps.stream().filter(Optional::isPresent).toList().size() != data.items().size())
-            throw new DomainValidationException("Provided data violates domain constraints");
-        else{
-            Discount discount = discountOps.get();
-            List<Item> items = itemsOps.stream().filter(Optional::isPresent).map(Optional::get).toList();
-
-            return discountCalculationService.getDiscounts(items, discount);
-        }
+        return discountCalculationService.getDiscounts(data);
     }
 
-//    public void run() throws DomainValidationException {
-//
-//        Data data = view.getData();
-//
-//        if(data == null || data.items() == null)
-//            throw new DomainValidationException("Data.items is null");
-//
-//
-//        Optional<Discount> discountOps = Discount.of(data.discount());
-//
-//        List<Optional<Item>> itemsOps =
-//                data.items()
-//                        .entrySet()
-//                        .stream()
-//                        .map(entry -> Item.of(entry.getKey(), entry.getValue()))
-//                        .toList();
-//
-//        if(discountOps.isEmpty() || itemsOps.stream().filter(Optional::isPresent).toList().size() != data.items().size())
-//            throw new DomainValidationException("Provided data violates domain constraints");
-//        else{
-//            Discount discount = discountOps.get();
-//            List<Item> items = itemsOps.stream().filter(Optional::isPresent).map(Optional::get).toList();
-//
-//            Map<String, Integer> discounts = discountCalculationService.getDiscounts(items, discount);
-//
-//            view.showResult(discounts);
-//        }
-//    }
 }
