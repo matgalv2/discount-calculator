0333ab991a7d4bf9c98be10d18ad46137e3e898e
mateusz <matgalv2@gmail.com>
2024-10-20 13:46:32 +0200

    Updated

--- src/main/java/io/github/g4lowy/view/DiscountCalculationViewConsole.java
+++ src/main/java/io/github/g4lowy/view/DiscountCalculationViewConsole.java
@@ -2,18 +2,22 @@ package io.github.g4lowy.view;
 
 import io.github.g4lowy.dto.Data;
 
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Random;
 
 public class DiscountCalculationViewConsole implements DiscountCalculationView {
 
     private static final int [] DISCOUNTS = new int []{100, 125, 150, 175};
-    private static final Map<String, Integer> ITEMS_PRICE = Map.of(
-            "Product1", 130,
-            "Product2", 150,
-            "Product3", 175,
-            "Product4", 250);
+    private static final LinkedHashMap<String, Integer> ITEMS_PRICE;
 
+    static {
+        ITEMS_PRICE = new LinkedHashMap<>();
+        ITEMS_PRICE.put("Product1", 130);
+        ITEMS_PRICE.put("Product2", 150);
+        ITEMS_PRICE.put("Product3", 175);
+        ITEMS_PRICE.put("Product4", 250);
+    }
 
     @Override
     public Data getData() {
