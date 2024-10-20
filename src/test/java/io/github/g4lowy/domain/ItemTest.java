a7da0ba9223aae5caf136c3e530b2e46ab2f60e0
mateusz <matgalv2@gmail.com>
2024-10-20 14:42:38 +0200

    Added tests for key field

--- src/test/java/io/github/g4lowy/domain/ItemTest.java
+++ src/test/java/io/github/g4lowy/domain/ItemTest.java
@@ -10,19 +10,37 @@ public class ItemTest {
 
     @Test
     public void negativeDiscount() {
-        Optional<Item> item = Item.of("", -1);
+        Optional<Item> item = Item.of("p1", -1);
         assertTrue(item.isEmpty());
     }
 
     @Test
     public void zeroDiscount() {
-        Optional<Item> item = Item.of("", 0);
+        Optional<Item> item = Item.of("p1", 0);
         assertTrue(item.isEmpty());
     }
 
     @Test
     public void positiveDiscount() {
-        Optional<Item> item = Item.of("", 1);
+        Optional<Item> item = Item.of("p1", 1);
         assertTrue(item.isPresent());
+    }
+
+    @Test
+    public void nullName() {
+        Optional<Item> item = Item.of(null, 0);
+        assertTrue(item.isEmpty());
+    }
+
+    @Test
+    public void blankName() {
+        Optional<Item> item = Item.of(" ", 0);
+        assertTrue(item.isEmpty());
+    }
+
+    @Test
+    public void emptyName() {
+        Optional<Item> item = Item.of("", 0);
+        assertTrue(item.isEmpty());
     }
 }
